0f6d637e871426eeef10fb9e35197adcda3ac7b7
Paul Kepinski <paul.kepinski@orange.fr>
2022-07-23 11:58:52 +0200

    Fix all return http codes

--- src/EventSubscriber/KernelSubscriber.php
+++ src/EventSubscriber/KernelSubscriber.php
@@ -7,36 +7,31 @@ namespace Jupi\DropzoneJsUploaderBundle\EventSubscriber;
 use Jupi\DropzoneJsUploaderBundle\Service\UploadManager;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class KernelSubscriber implements EventSubscriberInterface
 {
-    private bool $exception = false;
-
     public function __construct(
         private UploadManager $uploader
     ) {
     }
 
-    public function onKernelException(ExceptionEvent $event): void
-    {
-        $this->exception = true;
-    }
-
     public function onKernelResponse(ResponseEvent $event): void
     {
-        if ($this->uploader->isCurrentRequestHandled() && ($this->exception || $this->uploader->isUploadSoftFailed())) {
-            // Return an empty response to display the default Dropzone "500" error message
-            $event->setResponse(new Response('', 500));
+        if ($this->uploader->isCurrentRequestHandled()) {
+            // Return an empty response to fix the broken Dropzone error message
+            $event->setResponse(new Response('',
+                $this->uploader->isUploadSoftFailed() && 200 === $event->getResponse()->getStatusCode()
+                    ? 500
+                    : $event->getResponse()->getStatusCode()
+            ));
         }
     }
 
     public static function getSubscribedEvents()
     {
         return [
-            KernelEvents::EXCEPTION => 'onKernelException',
             KernelEvents::RESPONSE => 'onKernelResponse',
         ];
     }
